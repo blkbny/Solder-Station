6a2fe72afb3560082bf3db0c3396954030bacd00
David <Drperreault1990@gmail.com>
2015-12-15 16:03:02 -0500

    added basic proto code

---
+++ new version/code/SolderStation-c-code1.0/SolderStation-c-code1.0/main.c
@@ -0,0 +1,28 @@
+/*
+ * SolderStation-c-code1.0.c
+ *
+ * Created: 12/15/2015 2:21:16 PM
+ * Author : Dperreault
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
+void init_hardwr(void)
+{
+	//init PWM 
+	//init ADC0
+	//init ADC1
+	//init SPI
+	//init pins PA0-PA1
+}
+
+
